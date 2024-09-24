99bcfdd937026ea3667e32f6a2e180a96391b55c
thespecialone1 <s3neo36@gmail.com>
2024-09-25 02:30:51 +0400

    tried to turn off system signals e.g. Ctrl+z but unsucessfull for now

--- t.go
+++ t.go
@@ -18,21 +18,20 @@ func main() {
 		}
 		defer term.Restore(int(os.Stdin.Fd()), oldstate)
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		signal.Ignore(syscall.SIGINT, syscall.SIGTERM)	// Not Working
+	sigChan := make(chan os.Signal, 1)		//chan o.Signal will listen for signals
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM) //Registers the channel to receive notifications
 	go func() {
 		<-sigChan
 		term.Restore(int(os.Stdin.Fd()), oldstate)
 		os.Exit(0)
 	}()
-
-		fmt.Print("press :q to quit.")
+		fmt.Print("press :q to quit. \n")
 
 	// buff := make([]byte,1) if use this then no need to define c
 	var c byte
 	buff := []byte{c}
 	var inputBuffer []byte
-
 	for  {
 		_, err := os.Stdin.Read(buff)
 		if err!= nil {
@@ -40,15 +39,14 @@ func main() {
 			return
 		}
 		fmt.Printf("%c", buff[0])
-
 		// Append the charector into buffer
 		inputBuffer =append(inputBuffer, buff[0])
 		// to check if the charectors entered are :q
 		if len(inputBuffer)>= 2 && string(inputBuffer[len(inputBuffer)-2:]) == ":q" {
-		fmt.Println("You pressed :q : ")	
 		break
 		}
 	}
+	
 	//Restore terminal and exit
 	term.Restore(int(os.Stdin.Fd()), oldstate)
 }
